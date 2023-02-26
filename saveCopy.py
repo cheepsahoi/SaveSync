6ab698ccecf7f489b16c6ce6e83f96192a43bdc5
Rob <99067960+cheepsahoi@users.noreply.github.com>
2023-02-26 22:43:36 +0100

    Start scripts for web server and save behavior

---
+++ saveCopy.py
@@ -0,0 +1,6 @@
+# Ask user if on home ethernet or WiFi then do either copy or hardlink
+import urllib.request # urllib.request.urlretrieve(url, location/name)
+
+localLocation = ""
+remoteLocation = ""
+
