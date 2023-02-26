962b184548534a07bbdbfe05a7b18847c29e5d32
Rob <99067960+cheepsahoi@users.noreply.github.com>
2023-02-26 19:34:58 +0100

    Add check for user's operating system to call for appropriate WiFi check command

--- getWiFi.py
+++ getWiFi.py
@@ -1,24 +1,46 @@
 # Get WiFI SSID
-SystemOS = prompt("What OS? MacOS (M) or Windows (W)? : ")
+import subprocess
+
+def getSystemOS():
+    try:
+        subprocess.run(["ls"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        os = "linux"
+        print("Linux/MacOS detected.")
+    except FileNotFoundError:
+        os = "windows"
+        print("Windows detected.")
+    return os
 
 # Get SSID for specified System OS
 def WifiSSID(SystemOS):
-    # For MacOS
-    if SystemOS.lower() == "m":
-        import subprocess
+    # For Linux/MacOS
+    if SystemOS == "linux":
         output = subprocess.check_output(["/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport", "-I"])
         output = output.decode("utf-8")
-
         for line in output.split("\n"):
             if "SSID: " in line:
                 ssid = line.split(": ")[1]
                 print(ssid)
     # For Windows OS
-    elif SystemOS.lower() == "l":
-        import wlanapi
-        wlan = wlanapi.WLANAPI()
-        interface_guid = wlan.get_interface_guid()
-        current_network = wlan.get_current_network(interface_guid)
-        ssid = current_network.get("ssid")
+    elif SystemOS == "windows":
+        print("subprocess imported")
+        output = subprocess.check_output(["netsh", "wlan", "show", "interfaces"])
+        print("First output")
+        output = output.decode("utf-8")
+        print("Second output")
 
-WifiSSID()
+        for line in output.split("\n"):
+            if "SSID" in line:
+                ssid = line.split(":")[1].strip()
+                print(ssid)
+
+        # import wlanapi
+        # print("wlanapi imported. Press enter.")
+        # wlan = wlanapi.WLANAPI()
+        # interface_guid = wlan.get_interface_guid()
+        # current_network = wlan.get_current_network(interface_guid)
+        # ssid = current_network.get("ssid")
+
+
+WifiSSID(getSystemOS())
+input("Press enter to exit.")
