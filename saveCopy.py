423a0f7aae3ad3561fbbd0b14b1537871db153fa
Rob <99067960+cheepsahoi@users.noreply.github.com>
2023-02-28 23:50:10 +0100

    Add test file for GUI

--- saveCopy.py
+++ saveCopy.py
@@ -1,6 +1,28 @@
-# Ask user if on home ethernet or WiFi then do either copy or hardlink
-import urllib.request # urllib.request.urlretrieve(url, location/name)
+import os
+def getGame():
+    gameList = ["Hogwarts Legacy", "Spider-Man"]
+    index = 1
+    for game in gameList:
+        print(str(index) + ". " + game)
+        index += 1
+    while True:
+        game = input("\nSelect game: ")
+        try:
+            game = gameList[int(game) - 1]
+            break
+        except:
+            print("Choose from the provided games.")
+    print("Game chosen is " + game)
 
-localLocation = ""
-remoteLocation = ""
+localLocation = input("Game save location: ")
+remoteLocation = "site/upload"
+saveFile = "test"
+
+# Download the file #urllib.request.urlretrieve(remote, local)
+# wget -r -nH -R html,tmp *link*
+# curl -X POST http://127.0.0.1:8000/upload -F 'files=@multiple-example-1.txt' -F 'files=@multiple-example-2.txt'
+print("Downloading from: " + remoteLocation + "/download/")
+print("Saving to: " + saveFile)
+
+getGame()
 
---
+++ test.py
@@ -0,0 +1,89 @@
+import tkinter as tk
+import urllib.request
+import os
+import re
+from tkinter import filedialog
+
+class DownloadGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Download and Upload Files")
+        
+        # Create URL input box
+        self.url_label = tk.Label(master, text="Enter URL:")
+        self.url_label.grid(row=0, column=0, sticky="w")
+        self.url_entry = tk.Entry(master, width=50)
+        self.url_entry.grid(row=0, column=1, padx=5, pady=5)
+        
+        # Create Save location input box
+        self.save_label = tk.Label(master, text="Enter save location:")
+        self.save_label.grid(row=1, column=0, sticky="w")
+        self.save_entry = tk.Entry(master, width=50)
+        self.save_entry.grid(row=1, column=1, padx=5, pady=5)
+        
+        # Create Download button
+        self.download_button = tk.Button(master, text="Download Files", command=self.download_files)
+        self.download_button.grid(row=2, column=1, padx=5, pady=5)
+        
+        # Create Upload button
+        self.upload_button = tk.Button(master, text="Upload Files", command=self.upload_files)
+        self.upload_button.grid(row=3, column=1, padx=5, pady=5)
+        
+        # Create status label
+        self.status_label = tk.Label(master, text="")
+        self.status_label.grid(row=4, column=1, padx=5, pady=5)
+    
+    def download_files(self):
+        url = self.url_entry.get()
+        save_location = self.save_entry.get()
+        self.status_label.config(text="Downloading files from " + url)
+        
+        # Make request to URL
+        try:
+            response = urllib.request.urlopen(url)
+            html = response.read().decode('utf-8')
+        except:
+            self.status_label.config(text="Error connecting to server")
+            return
+        
+        # Find all links on the page
+        links = re.findall(r'href=[\'"]?([^\'" >]+)', html)
+        
+        # Delete any existing files in the save location
+        for file_name in os.listdir(save_location):
+            file_path = os.path.join(save_location, file_name)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                print(e)
+        
+        # Download each file
+        for link in links:
+            file_url = url + link
+            file_name = os.path.join(save_location, link)
+            urllib.request.urlretrieve(file_url, file_name)
+        
+        self.status_label.config(text="All files downloaded to " + save_location)
+    
+    def upload_files(self):
+        url = self.url_entry.get() + '/upload'
+        file_paths = filedialog.askopenfilenames(title="Select files to upload")
+        if len(file_paths) == 0:
+            self.status_label.config(text="No files selected")
+            return
+        
+        self.status_label.config(text="Uploading files to " + url)
+        
+        # Upload each file
+        for file_path in file_paths:
+            file_name = os.path.basename(file_path)
+            file_url = os.path.join(url, file_name)
+            with open(file_path, 'rb') as file:
+                response = urllib.request.urlopen(file_url, file.read())
+        
+        self.status_label.config(text="All files uploaded to " + url)
+
+root = tk.Tk()
+download_gui = DownloadGUI(root)
+root.mainloop()
