366d46cbe252cf225633bb00a44bb61cc90d6c9f
Rob <99067960+cheepsahoi@users.noreply.github.com>
2023-03-01 00:41:01 +0100

    Remove URL box

--- test.py
+++ test.py
@@ -7,13 +7,7 @@ from tkinter import filedialog
 class DownloadGUI:
     def __init__(self, master):
         self.master = master
-        master.title("Download and Upload Files")
-        
-        # Create URL input box
-        self.url_label = tk.Label(master, text="Enter URL:")
-        self.url_label.grid(row=0, column=0, sticky="w")
-        self.url_entry = tk.Entry(master, width=50)
-        self.url_entry.grid(row=0, column=1, padx=5, pady=5)
+        master.title("Sync Saves")
         
         # Create Save location input box
         self.save_label = tk.Label(master, text="Enter save location:")
@@ -32,13 +26,8 @@ class DownloadGUI:
         # Create status label
         self.status_label = tk.Label(master, text="")
         self.status_label.grid(row=4, column=1, padx=5, pady=5)
-    
-    # Ask user what game to determine the URL to use
-    def getUrl(self):
-
 
     def download_files(self):
-        url = self.url_entry.get()
         save_location = self.save_entry.get()
         self.status_label.config(text="Downloading files from " + url)
         
@@ -71,27 +60,21 @@ class DownloadGUI:
         self.status_label.config(text="All files downloaded to " + save_location)
     
     def upload_files(self):
-        #url = self.url_entry.get() + '/upload'
-        url = 'http://46.232.211.40:9911/upload' #tmp
-        #save_location = self.save_entry.get()
+        save_location = self.save_entry.get()
         save_location = '/Users/rob/Downloads/http_download' #tmp
         self.status_label.config(text="Uploading files to " + url)
         
         # Upload each file in save location
         files = []
+        uploadUrl = url + '/upload'
         for file_name in os.listdir(save_location):
             file_path = os.path.join(save_location, file_name)
             print("Current file: " + file_path) #tmp
             if os.path.isfile(file_path):
-                #file_url = os.path.join(url, file_name)
-                files.append(str("-F files=@" + file_path)) #tmp
-                #print(files) #tmp
-                #input("Paused.")#tmp
-                #with open(file_path, 'rb') as file:
-                #    response = urllib.request.urlopen(file_url, file.read())
+                files.append(str("-F files=@" + file_path))
         files = " ".join(files)
-        os.system("curl -X POST " + url + " " + files)
-        self.status_label.config(text="All files uploaded to " + url)
+        os.system("curl -X POST " + uploadUrl + " " + files)
+        self.status_label.config(text="All files uploaded to " + uploadUrl)
 
 root = tk.Tk()
 download_gui = DownloadGUI(root)
