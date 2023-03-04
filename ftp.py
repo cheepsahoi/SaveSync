8cdb49445723ec7e1bc71da9c76f283fc71c325f
Rob <99067960+cheepsahoi@users.noreply.github.com>
2023-03-04 18:12:42 +0100

    Add upload to remote function

--- ftp.py
+++ ftp.py
@@ -22,7 +22,7 @@ def downloadFiles(game, saves_path):
         print("FTP failed")
         exit()
 
-    # Change directory to game's directory
+    # Change directory to game's remote directory
     print("Opening FTP game directory")
     try:
         ftp.cwd(game)
@@ -42,6 +42,64 @@ def downloadFiles(game, saves_path):
         except:
             print("Failed to download " + file)
     print("Complete download")
+    # Delete extra files in local that's not in remote
+    for file in os.listdir():
+        try:
+            ftp.nlst().index(file)
+        except ValueError:
+            print(file + " not in remote. Deleting from local")
+            os.remove(file)
+        except:
+            print("Something went wrong. Exiting.")
+            exit()
+
+    # Close FTP connection
+    print("Closing FTP connection")
+    ftp.quit()
+
+def uploadFiles(game, saves_path):
+    # Go to game's save directory
+    print("Opening directory: " + saves_path)
+    os.chdir(saves_path)
+
+    # Connect to FTP
+    print("Establishing FTP connection")
+    try:
+        ftp = ftplib.FTP(ftp_address)
+        ftp.connect(port=ftp_port)
+        ftp.login() # Anonymous login
+        print("FTP connected")
+    except:
+        print("FTP failed")
+        exit()
+
+    # Change directory to game's remote directory
+    print("Opening FTP game directory")
+    try:
+        ftp.cwd(game)
+    except:
+        print("Directory do not exist. Create folder in remote location first")
+        exit()
+    # For each file in local directory, upload it
+    print("Upload files from local directory for " + game)
+    for file in os.listdir():
+        try:
+            ftp.storbinary("STOR " + file, open(file, "rb"))
+            print(file + " uploaded")
+        except:
+            print("Failed to upload " + file)
+    print("Complete upload")
+    # Delete extra save files in remote folder that's not in sync with local
+    for file in ftp.nlst():
+        try:
+            print("Checking if " + file + " also exist in local")
+            os.listdir().index(file)
+        except ValueError:
+            print(file + " not in local. Deleting")
+            ftp.delete(file)
+        except:
+            print("Something went wrong. Exiting.")
+            exit()
     # Close FTP connection
     print("Closing FTP connection")
     ftp.quit()
